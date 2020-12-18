ad1ad06032423139aed0b5c4434346d32c50b798
zangtengfei <81709456@qq.com>
2020-12-18 15:24:35 +0800

    修正版本对比方法

--- miniprogram/pages/upgrade/upgrade.js
+++ miniprogram/pages/upgrade/upgrade.js
@@ -20,13 +20,13 @@ Page({
           .find()
           .then(devices => {
             if(devices) {
-              console.log(devices);
               let devList = [];
               devices.forEach(device => {
                 devList.push({
                   id: device.get('objectId'),
                   sn: device.get('deviceSN'),
                   version: device.get('versionNO'),
+                  needUpdate: this.compareVersion(device.get('versionNO'), this.data.latestVersion),
                   workStatus: device.get('workStatus'),
                   networkType: device.get('networkType'),
                   isAutoUpdate: device.get('isAutoUpdate')
@@ -41,6 +41,38 @@ Page({
     this.loop = setInterval(() => {
       this.getDeviceList();
     }, 20000);
+  },
+  compareVersion(version1, version2) {
+    const arrayA = version1.split('.');
+    const arrayB = version2.split('.');
+
+    let pointer = 0;
+    while (pointer < arrayA.length && pointer < arrayB.length) {
+        const res = arrayA[pointer] - arrayB[pointer];
+        if (res === 0) {
+            pointer++;
+        } else {
+            return res > 0 ? 1 : -1;
+        }
+    }
+    // 若arrayA仍有小版本号
+    while (pointer < arrayA.length) {
+        if (+arrayA[pointer] > 0) {
+            return 1;
+        } else {
+            pointer++;
+        }
+    }
+    // 若arrayB仍有小版本号
+    while (pointer < arrayB.length) {
+        if (+arrayB[pointer] > 0) {
+            return -1;
+        } else {
+            pointer++;
+        }
+    }
+    // 版本号完全相同
+    return 0;
   },
   addDevice() {
     wx.scanCode({
@@ -125,6 +157,7 @@ Page({
               id: device.get('objectId'),
               sn: device.get('deviceSN'),
               version: device.get('versionNO'),
+              needUpdate: this.compareVersion(device.get('versionNO'), this.data.latestVersion),
               workStatus: device.get('workStatus'),
               networkType: device.get('networkType'),
               isAutoUpdate: device.get('isAutoUpdate')
